8c261360e669f9a44d3a01dd8b4ecf61043e117f
greatericontop <greateric@protonmail.com>
2023-09-08 19:18:40 -0400

    game is basically done

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pygame
 
 pygame.init()
 pygame.display.set_caption("Tic Tac Toe by greateric")
-pygame_canvas = pygame.display.set_mode((1200, 800))
+canvas = pygame.display.set_mode((1200, 800))
 
 # "Times New Roman", "Courier New", "Ubuntu Mono", etc.
 my_font = pygame.font.SysFont('Calibri', 36)
@@ -24,7 +24,7 @@ board = [
     ['none', 'none', 'none'],
     ['none', 'none', 'none'],
 ]
-turn = 'x'
+turn = 'X'
 
 
 #####
@@ -37,10 +37,10 @@ def draw_centered_text(canvas: pygame.Surface, text: pygame.Surface, x: float, y
     canvas.blit(text, (x - text_rect.width/2, y - text_rect.height/2))
 
 
-def draw_board(canvas):
-    global board
+def draw_board():
+    global board, canvas
     canvas.fill(0x0000aa)
-    draw_centered_text(canvas, my_font.render('Tic Tac Toe', True, 0xffffffff), 600, 50)
+    draw_centered_text(canvas, my_font.render('Tic Tac Toe', True, 0xffffffff), 600, 30)
     pygame.draw.rect(canvas, 0x000000, (495, 100, 10, 600))
     pygame.draw.rect(canvas, 0x000000, (695, 100, 10, 600))
     pygame.draw.rect(canvas, 0x000000, (300, 295, 600, 10))
@@ -49,16 +49,44 @@ def draw_board(canvas):
         for y in (0, 1, 2):
             x_coordinate = 400 + 200*x
             y_coordinate = 200 + 200*y
-            if board[y][x] == 'x':
+            if board[y][x] == 'X':
                 draw_centered_text(canvas, my_font.render('X', True, 0xff5555ff), x_coordinate, y_coordinate)
-            elif board[y][x] == 'o':
+            elif board[y][x] == 'O':
                 draw_centered_text(canvas, my_font.render('O', True, 0xffff55ff), x_coordinate, y_coordinate)
+    winner = check_win()
+    if winner == 'X':
+        draw_centered_text(canvas, my_font.render('X won!', True, 0xff5555ff), 600, 80)
+    elif winner == 'O':
+        draw_centered_text(canvas, my_font.render('O won!', True, 0xffff55ff), 600, 80)
+
+
+def check_win():
+    global board
+    for winner in ('X', 'O'):
+        if (
+            # rows
+            board[0][0] == board[0][1] == board[0][2] == winner
+            or board[1][0] == board[1][1] == board[1][2] == winner
+            or board[2][0] == board[2][1] == board[2][2] == winner
+            # columns
+            or board[0][0] == board[1][0] == board[2][0] == winner
+            or board[0][1] == board[1][1] == board[2][1] == winner
+            or board[0][2] == board[1][2] == board[2][2] == winner
+            # diagonals
+            or board[0][0] == board[1][1] == board[2][2] == winner
+            or board[0][2] == board[1][1] == board[2][0] == winner
+        ):
+            return winner
+    return 'none'
 
 
 def mouse_button_press(pos):
-    global board, turn
+    global board, canvas, turn
     x = pos[0]
     y = pos[1]
+    if check_win() != 'none':
+        # Don't allow the user to click if the game is over.
+        return
     # Check X
     if 310 <= x <= 490:
         board_x = 0
@@ -81,7 +109,7 @@ def mouse_button_press(pos):
         return
     if board[board_y][board_x] == 'none':
         board[board_y][board_x] = turn
-        turn = 'x' if turn == 'o' else 'o'
+        turn = 'X' if turn == 'O' else 'O'
 
 
 #####
@@ -96,7 +124,7 @@ while True:
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_button_press(event.pos)
-    draw_board(pygame_canvas)
+    draw_board()
     pygame.display.update()
 
 
